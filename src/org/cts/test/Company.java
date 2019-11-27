8d415b821edc26f0e381a03c8b1329f65394b26d
indujaudhayak <indujaudhayak@gmail.com>
2019-11-27 16:40:13 +0530

    askjas

--- src/org/cts/test/Company.java
+++ src/org/cts/test/Company.java
@@ -9,13 +9,12 @@ public class Company  {
 		Company c =new Company();
 		c.compid();
 SingletonClass.add();
-<<<<<<< HEAD
-SingletonClass.empid();	
-Thread.sleep(10000);
-=======
+
+
 SingletonClass.empid();
 System.out.println("2345667");
->>>>>>> 70a0de479a92b4f0e82018e34ccce074b2724dad
+SingletonClass.empid();	
+Thread.sleep(10000);
 	}
 
 }
